3f7ec1a38275a5698b11ebb6eae5cb0cc7f1401f
Jvbs0504 <joaovbs0504@gmail.com>
2024-12-04 19:13:59 -0300

    Created Grid Scriptable Object

---
+++ flipcube/Assets/Game/Scripts/Scriptable/SCR_GridInfo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Board", menuName = "Board/New Grid")]
+public class SCR_GridInfo : ScriptableObject
+{
+   [Header("Level Status")]
+   [SerializeField] int level;
+   [SerializeField] int width;
+   [SerializeField] int height;
+
+    public int GetName()
+    {
+        return level;
+    }
+    public int GetWidth()
+    {
+        return width;
+    }
+    public int GetHeight()
+    {
+        return height;
+    }
+}

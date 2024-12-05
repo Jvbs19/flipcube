8ee9efed1e81577381dfb117a3642e5667203a14
Jvbs0504 <joaovbs0504@gmail.com>
2024-12-05 14:33:31 -0300

    Implemented GameState Logic

---
+++ flipcube/Assets/Game/Scripts/Managers/SCR_GameState.cs
@@ -0,0 +1,36 @@
+public enum GameState
+{
+    wait,
+    move
+}
+
+public  static class SCR_GameState
+{
+
+    static GameState _currentGameState = GameState.move;
+    public static void SetCurrentGameState(GameState state)
+    {
+        _currentGameState = state;
+    }
+
+    public static GameState CheckCurrentGameState()
+    {
+        return _currentGameState;
+    }
+
+    public static bool IsGameStateWait()
+    {
+        if (_currentGameState == GameState.wait)
+            return true;
+
+        return false;
+    }
+
+    public static bool IsGameStateMove()
+    {
+        if (_currentGameState == GameState.move)
+            return true;
+
+        return false;
+    }
+}

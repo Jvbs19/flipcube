d4fa4de87dc6c815e917aa302f7068cb4e391cc1
Jvbs0504 <joaovbs0504@gmail.com>
2024-12-06 20:45:09 -0300

    Implemented Main Menu, Tutorial and Level Selection Screen

--- flipcube/Assets/Game/Scripts/Managers/SCR_SceneManager.cs
+++ flipcube/Assets/Game/Scripts/Managers/SCR_SceneManager.cs
@@ -5,10 +5,20 @@ public class SCR_SceneManager : MonoBehaviour
 {
     public void LoadSceneByName(string name)
     {
+        SCR_GameState.SetCurrentGameState(GameState.move);
+        SCR_GameStatus.ResetAll();
         SceneManager.LoadScene(name);
+    }
+    public void LoadSceneByNextIndex()
+    {
+        SCR_GameState.SetCurrentGameState(GameState.move);
+        SCR_GameStatus.ResetAll();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
     public void ResetCurrentScene()
     {
+        SCR_GameState.SetCurrentGameState(GameState.move);
+        SCR_GameStatus.ResetAll();
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
     public void QuitGame()
